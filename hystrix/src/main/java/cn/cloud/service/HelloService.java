673196a09a9df512268276c77674eef83785e539
薛紫浩 <17631196617@163.com>
2019-11-29 19:16:43 +0800

    sys

---
+++ hystrix/src/main/java/cn/cloud/service/HelloService.java
@@ -0,0 +1,24 @@
+package cn.cloud.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @HystrixCommand(fallbackMethod = "hiError")
+    public String hiService(String name) {
+        String url = "http://SERVICE-HI/hi?name=" + name;
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    public String hiError(String name) {
+        return "hi," + name + ",sorry,error!";
+    }
+
+}
