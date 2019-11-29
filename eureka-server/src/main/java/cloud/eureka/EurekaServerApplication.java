e0dd9ba2a77a9c687c811628488782d6a646e97d
薛紫浩 <17631196617@163.com>
2019-11-29 19:19:42 +0800

    sys
    sys

--- eureka-server/src/main/java/cloud/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/cloud/eureka/EurekaServerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaServer
 @RestController
+//注册中心
 public class EurekaServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
